48b259761230cacbca3dee59dd7bd4d50e958fe3
RRevkoUA <rustamanrevko@gmail.com>
2024-08-05 23:35:26 +0300

    add signToken in auth service, and add JWT_SECRET to env

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,23 @@
 import { Injectable } from '@nestjs/common';
-import { AuthDto } from './dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService { }
+export class AuthService {
+  constructor(private jwt: JwtService) {}
+
+  async signToken(userId: number, email: string): Promise<object> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: process.env.JWT_SECRET,
+    });
+
+    return {
+      access_token: token,
+    };
+  }
+}
