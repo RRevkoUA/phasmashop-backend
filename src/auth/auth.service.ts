112b0d52a3a1a383fca29f20c2070a50cfecb37f
RRevkoUA <rustamanrevko@gmail.com>
2024-08-06 00:10:54 +0300

    Add exception in duplicate

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as argon from 'argon2';
 import { AuthDto } from './dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from 'src/schemas/users.schema';
-import { Model, Types } from 'mongoose';
+import { Model, MongooseError, Types } from 'mongoose';
 
 @Injectable()
 export class AuthService {
@@ -14,12 +14,19 @@ export class AuthService {
   ) {}
 
   async signup(dto: AuthDto) {
-    const token = await argon.hash(dto.password);
-    const user = await this.userModule.create({
-      ...dto,
-      hash: token,
-    });
-    return this.signToken(user._id, user.email);
+    try {
+      const token = await argon.hash(dto.password);
+      const user = await this.userModule.create({
+        ...dto,
+        hash: token,
+      });
+      return this.signToken(user._id, user.email);
+    } catch (err) {
+      if (err.code === 11000) {
+        throw new ForbiddenException('this username or email already in use');
+      }
+      throw err;
+    }
   }
 
   async signin(dto: AuthDto) {
