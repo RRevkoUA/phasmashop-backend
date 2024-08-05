f19ae4175ddcee72fd070fb2d932306c9ce410ee
RRevkoUA <rustamanrevko@gmail.com>
2024-08-05 21:53:49 +0300

    delete validators from schema

--- src/schemas/users.schema.ts
+++ src/schemas/users.schema.ts
@@ -1,5 +1,4 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { IsEmail, IsPhoneNumber } from 'class-validator';
 import { Types } from 'mongoose';
 
 @Schema({ timestamps: true })
@@ -7,14 +6,12 @@ export class User {
   @Prop({ unique: true, required: true })
   username: string;
 
-  @IsEmail()
   @Prop({ unique: true, required: true })
   email: string;
 
   @Prop({ required: true })
   hash: string;
 
-  @IsPhoneNumber()
   @Prop()
   phone: string;
 
