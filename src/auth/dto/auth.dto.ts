9547694a5fd6f66e07c731d30005edd78841accb
RRevkoUA <rustamanrevko@gmail.com>
2024-08-05 23:32:35 +0300

    WIP: reconfigure dto

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,1 +1,42 @@
-export class AuthDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsEnum,
+  IsOptional,
+  IsPhoneNumber,
+  IsString,
+} from 'class-validator';
+
+export class AuthDto {
+  @IsString()
+  @ApiProperty({
+    default: 'user',
+  })
+  username: string;
+
+  @IsEmail()
+  @ApiProperty({
+    default: 'email@example.com',
+  })
+  email: string;
+
+  @IsString()
+  @ApiProperty({
+    default: 'password',
+  })
+  password: string;
+
+  @IsPhoneNumber()
+  @IsOptional()
+  @ApiProperty({
+    default: '+380985052935',
+  })
+  phone?: string;
+
+  @IsEnum(['Admin', 'Moderator', 'User', 'Guest'])
+  @IsOptional()
+  @ApiProperty({
+    default: 'User',
+  })
+  role?: string;
+}
