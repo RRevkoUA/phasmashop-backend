9002df3bb2b427df98c87bcc53c4fa6ab44a58a6
RRevkoUA <rustamanrevko@gmail.com>
2024-08-05 22:21:22 +0300

    recovery of accidentally deleted users service

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsEmail,
   IsEnum,
@@ -8,19 +9,24 @@ import {
 
 export class CreateUserDto {
   @IsString()
+  @ApiProperty()
   username: string;
 
   @IsEmail()
+  @ApiProperty()
   email: string;
 
   @IsString()
+  @ApiProperty()
   hash: string;
 
   @IsPhoneNumber()
   @IsOptional()
+  @ApiProperty()
   phone?: string;
 
   @IsEnum(['Admin', 'Moderator', 'User', 'Guest'])
   @IsOptional()
+  @ApiProperty()
   role?: string;
 }
---
+++ src/users/users.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from 'src/schemas/users.schema';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModule: Model<User>) {}
+
+  create(dto: CreateUserDto) {
+    this.userModule.create(dto);
+    return `This action adds a new user ${dto.username}`;
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
