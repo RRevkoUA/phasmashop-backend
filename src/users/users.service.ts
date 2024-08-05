e92df845059d572b4400ae6e553d735345839fba
RRevkoUA <rustamanrevko@gmail.com>
2024-08-05 22:26:03 +0300

    refactor CreateUser and UpdateUser dto's to index.dto

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from 'src/schemas/users.schema';
 import { Model } from 'mongoose';
+import { CreateUserDto, UpdateUserDto } from './dto';
 
 @Injectable()
 export class UsersService {
