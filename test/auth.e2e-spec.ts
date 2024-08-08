f37071d4b4dc79b5b3fe1f3de0c437cf7b6f40b4
RRevkoUA <rustamanrevko@gmail.com>
2024-08-08 20:35:30 +0300

    add signin tests

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -1,13 +1,18 @@
 import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from 'src/app.module';
-import { SignupAuthDto, SigninAuthDto } from 'src/auth/dto';
+import { SignupAuthDto } from 'src/auth/dto';
 import * as pactum from 'pactum';
 import mongoose from 'mongoose';
 
 describe('AuthController E2E Test', () => {
   let app: INestApplication;
   const host = `http://localhost:${process.env.APP_PORT}/`;
+  const dto: SignupAuthDto = {
+    email: 'goored@mail.com',
+    password: 'goodpawwssword123',
+    username: 'Usernamgge123',
+  };
 
   beforeAll(async () => {
     const moduleRef: TestingModule = await Test.createTestingModule({
@@ -20,28 +25,22 @@ describe('AuthController E2E Test', () => {
         whitelist: true,
       }),
     );
-    await mongoose.connect(process.env.DB_URL);
     await app.init();
     await app.listen(process.env.APP_PORT);
-
     pactum.request.setBaseUrl(host);
   });
 
   afterAll(async () => {
+    await mongoose.connect(process.env.DB_URL);
     const db = mongoose.connection.db;
     await db.dropDatabase();
     mongoose.connection.close();
+
     app.close();
   });
 
   describe('Signup testing', () => {
     const uri = 'signup/';
-    const dto: SignupAuthDto = {
-      email: 'goored@mail.com',
-      password: 'goodpawwssword123',
-      username: 'Usernamgge123',
-    };
-
     it('Should signUp new user', () => {
       const path = uri;
       return pactum
@@ -113,6 +112,62 @@ describe('AuthController E2E Test', () => {
         .expectStatus(HttpStatus.BAD_REQUEST);
     });
     it('Should not signUp new user, because there is no data', () => {
+      const path = uri;
+      return pactum.spec().post(path).expectStatus(HttpStatus.BAD_REQUEST);
+    });
+  });
+
+  describe('Signin testing', () => {
+    const uri = 'signin/';
+    it('Should signIn user in created before user by email', () => {
+      const path = uri;
+      return pactum
+        .spec()
+        .post(path)
+        .withBody({
+          usernameOrEmail: dto.email,
+          password: dto.password,
+        })
+        .expectStatus(HttpStatus.OK);
+    });
+
+    it('Should signIn user in created before user by username', () => {
+      const path = uri;
+      return pactum
+        .spec()
+        .post(path)
+        .withBody({
+          usernameOrEmail: dto.username,
+          password: dto.password,
+        })
+        .expectStatus(HttpStatus.OK);
+    });
+
+    it('Should not signIn user in created before user by wrong password', () => {
+      const path = uri;
+      return pactum
+        .spec()
+        .post(path)
+        .withBody({
+          usernameOrEmail: dto.username,
+          password: 'rrrrrrrrrrrr',
+        })
+        .expectStatus(HttpStatus.FORBIDDEN);
+    });
+
+    it('Should not signIn user in created before user by unknown login', () => {
+      const path = uri;
+      return pactum
+        .spec()
+        .post(path)
+        .withBody({
+          usernameOrEmail: '24124124',
+          password: dto.password,
+        })
+        .expectStatus(HttpStatus.FORBIDDEN);
+    });
+
+    it('Should not signIn user in created before user, with empty body', () => {
       const path = uri;
       return pactum.spec().post(path).expectStatus(HttpStatus.BAD_REQUEST);
     });
