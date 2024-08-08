2be321f7130af695252a102df6a98f43d774d160
RRevkoUA <rustamanrevko@gmail.com>
2024-08-08 20:01:08 +0300

    add signup tests

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -24,36 +24,97 @@ describe('AuthController E2E Test', () => {
     await app.init();
     await app.listen(process.env.APP_PORT);
 
-    // for (const collection of collections) {
-    //   await db.dropCollection(collection.name);
-    // }
     pactum.request.setBaseUrl(host);
   });
 
   afterAll(async () => {
     const db = mongoose.connection.db;
-
     await db.dropDatabase();
     mongoose.connection.close();
     app.close();
   });
-  describe('auth', () => {
-    describe('Signup testing', () => {
-      const uri = 'signup/';
-      const dto: SignupAuthDto = {
-        email: 'goored@mail.com',
-        password: 'goodpawwssword123',
-        username: 'Usernamgge123',
-      };
 
-      it('Should signUp new user', () => {
-        const path = uri;
-        return pactum
-          .spec()
-          .post(path)
-          .withBody(dto)
-          .expectStatus(HttpStatus.CREATED);
-      });
+  describe('Signup testing', () => {
+    const uri = 'signup/';
+    const dto: SignupAuthDto = {
+      email: 'goored@mail.com',
+      password: 'goodpawwssword123',
+      username: 'Usernamgge123',
+    };
+
+    it('Should signUp new user', () => {
+      const path = uri;
+      return pactum
+        .spec()
+        .post(path)
+        .withBody(dto)
+        .expectStatus(HttpStatus.CREATED);
+    });
+
+    it('Should not signUp new user, because Email already taken', () => {
+      const path = uri;
+      return pactum
+        .spec()
+        .post(path)
+        .withBody({
+          email: dto.email,
+          password: dto.password,
+          username: 'NewUser',
+        })
+        .expectStatus(HttpStatus.FORBIDDEN);
+    });
+
+    it('Should not signUp new user, because Username already taken', () => {
+      const path = uri;
+      return pactum
+        .spec()
+        .post(path)
+        .withBody({
+          email: 'email@email.email',
+          password: dto.password,
+          username: dto.username,
+        })
+        .expectStatus(HttpStatus.FORBIDDEN);
+    });
+
+    it('Should not signUp new user, because unknown role taken', () => {
+      const path = uri;
+      return pactum
+        .spec()
+        .post(path)
+        .withBody({
+          ...dto,
+          role: 'UNKNOWN',
+        })
+        .expectStatus(HttpStatus.BAD_REQUEST);
+    });
+
+    it('Should not signUp new user, because invalid phone number format', () => {
+      const path = uri;
+      return pactum
+        .spec()
+        .post(path)
+        .withBody({
+          ...dto,
+          phone: '+3806669991112',
+        })
+        .expectStatus(HttpStatus.BAD_REQUEST);
+    });
+    it('Should not signUp new user, because invalid email format', () => {
+      const path = uri;
+      return pactum
+        .spec()
+        .post(path)
+        .withBody({
+          email: 'notaEmail',
+          password: dto.password,
+          username: dto.username,
+        })
+        .expectStatus(HttpStatus.BAD_REQUEST);
+    });
+    it('Should not signUp new user, because there is no data', () => {
+      const path = uri;
+      return pactum.spec().post(path).expectStatus(HttpStatus.BAD_REQUEST);
     });
   });
 });
