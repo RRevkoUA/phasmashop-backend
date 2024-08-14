1d5a22d3834f21d88d0b7a0dd3c4d8ea45274c05
RRevkoUA <rustamanrevko@gmail.com>
2024-08-15 02:05:11 +0300

    use cookieParser()

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,9 @@ import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger/dist/swagger-module';
 import { DocumentBuilder } from '@nestjs/swagger/dist/document-builder';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.setGlobalPrefix(process.env.APP_GLOBAL_PREFIX);
   const config = new DocumentBuilder()
     .setTitle('Phasmashop swagger api')
     .setDescription('pet project api testing, and representation')
@@ -21,6 +19,10 @@ async function bootstrap() {
       'refresh-token',
     )
     .build();
+
+  const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+  app.setGlobalPrefix(process.env.APP_GLOBAL_PREFIX);
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup(process.env.APP_SWAGGER_PATH, app, document);
 
