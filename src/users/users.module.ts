2d3d86491012569ebe940e9b93dbe1560b5a3b74
RRevkoUA <rustamanrevko@gmail.com>
2024-08-19 19:54:31 +0300

    add image schema to users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,6 +3,7 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from 'src/schemas/User.schema';
+import { Image, ImageSchema } from 'src/schemas/Image.schema';
 
 @Module({
   imports: [
@@ -10,6 +11,10 @@ import { User, UserSchema } from 'src/schemas/User.schema';
       {
         name: User.name,
         schema: UserSchema,
+      },
+      {
+        name: Image.name,
+        schema: ImageSchema,
       },
     ]),
   ],
