473570ecaf252cd8bb8a7a44e2e4454106ec13a2
RRevkoUA <rustamanrevko@gmail.com>
2024-08-21 13:07:06 +0300

    update subcategory dto

--- src/subcategory/dto/create-subcategory.dto.ts
+++ src/subcategory/dto/create-subcategory.dto.ts
@@ -1,5 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsString } from 'class-validator';
+
 export class CreateSubcategoryDto {
+  @IsString()
+  @ApiProperty({
+    example: 'Subcategory',
+    description: 'The name of the subcategory',
+    required: true,
+    default: 'SubCategory name',
+  })
   name: string;
-  isAvailable: boolean;
+
+  @IsBoolean()
+  @ApiProperty({
+    example: true,
+    description:
+      'The availability of the subcategory. If false, the subcategory, and ' +
+      'subobjects will not be displayed in the list of categories',
+    required: false,
+    default: false,
+  })
+  isAvailable?: boolean;
+
+  @IsString()
+  @ApiProperty({
+    example: 'New Category',
+    description: 'added this subcategory to exist category',
+    required: true,
+    default: 'Category',
+  })
   category: string;
 }
