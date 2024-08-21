7fa20584fad802deec563db177c6bd3acef65567
RRevkoUA <rustamanrevko@gmail.com>
2024-08-21 13:06:24 +0300

    import Mongoose and Category Module for subcategoty

--- src/subcategory/subcategory.module.ts
+++ src/subcategory/subcategory.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { SubcategoryService } from './subcategory.service';
 import { SubcategoryController } from './subcategory.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { SubcategorySchema, Subcategory } from 'src/common/schemas';
+import { CategoryModule } from 'src/category/category.module';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Subcategory.name,
+        schema: SubcategorySchema,
+      },
+    ]),
+    CategoryModule,
+  ],
   controllers: [SubcategoryController],
   providers: [SubcategoryService],
 })
