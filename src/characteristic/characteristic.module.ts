c81cb7a0f456f4afe0d15cc40f3397ed06eed259
RRevkoUA <rustamanrevko@gmail.com>
2024-08-21 17:15:33 +0300

    import Mongoose module

--- src/characteristic/characteristic.module.ts
+++ src/characteristic/characteristic.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { CharacteristicService } from './characteristic.service';
 import { CharacteristicController } from './characteristic.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Characteristic, CharacteristicSchema } from 'src/common/schemas';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Characteristic.name,
+        schema: CharacteristicSchema,
+      },
+    ]),
+  ],
   controllers: [CharacteristicController],
   providers: [CharacteristicService],
 })
