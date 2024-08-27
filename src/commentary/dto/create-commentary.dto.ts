82c6b4d83b7b1d3bef58e8fde08c464c5d09fae5
RRevkoUA <rustamanrevko@gmail.com>
2024-08-27 20:15:18 +0300

    update create commentary dto

--- src/commentary/dto/create-commentary.dto.ts
+++ src/commentary/dto/create-commentary.dto.ts
@@ -1,1 +1,16 @@
-export class CreateCommentaryDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
+
+export class CreateCommentaryDto {
+  @IsString()
+  @ApiProperty({
+    example: 'Commentary',
+    description: 'The text of the commentary',
+    required: true,
+    default: 'Commentary text',
+  })
+  text: string;
+
+  @IsString({ each: true })
+  images: string[];
+}
