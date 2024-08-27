e933113d60cc6531ab265af2756d54bffd3780b1
RRevkoUA <rustamanrevko@gmail.com>
2024-08-27 21:49:21 +0300

    add Mongoose module to module

--- src/commentary/commentary.module.ts
+++ src/commentary/commentary.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { CommentaryService } from './commentary.service';
 import { CommentaryController } from './commentary.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Comment, CommentSchema } from 'src/common/schemas';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Comment.name,
+        schema: CommentSchema,
+      },
+    ]),
+  ],
   controllers: [CommentaryController],
   providers: [CommentaryService],
 })
