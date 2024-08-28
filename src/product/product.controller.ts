e2c531b79b5ba82abcb2e2efd3fcb04485f95f74
RRevkoUA <rustamanrevko@gmail.com>
2024-08-28 20:05:56 +0300

    CRUD realization

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,33 +1,59 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto, UpdateProductDto } from './dto';
+import { ApiTags } from '@nestjs/swagger';
+import { ApiAccessAuth, GetUser } from 'src/common/decorator';
+import { JwtGuard } from 'src/common/guard';
+import { Document } from 'mongoose';
+import { User } from 'src/common/schemas';
 
+@UseGuards(JwtGuard)
+@ApiAccessAuth()
+@ApiTags('Product')
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
   @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productService.create(createProductDto);
+  create(
+    @Body() createProductDto: CreateProductDto,
+    @GetUser() user: User & Document,
+  ) {
+    return this.productService.create(createProductDto, user);
   }
 
+  @UseGuards()
   @Get()
   findAll() {
     return this.productService.findAll();
   }
 
+  @UseGuards()
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.productService.findOne(+id);
+    return this.productService.findOneById(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productService.update(+id, updateProductDto);
+  update(
+    @Param('id') id: string,
+    @Body() updateProductDto: UpdateProductDto,
+    @GetUser() user: User & Document,
+  ) {
+    return this.productService.update(id, updateProductDto, user);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.productService.remove(+id);
+  remove(@Param('id') id: string, @GetUser() user: User & Document) {
+    return this.productService.remove(id, user);
   }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,31 +1,88 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateProductDto, UpdateProductDto } from './dto';
 import { InjectModel } from '@nestjs/mongoose';
-import { Product } from 'src/common/schemas';
-import { Model } from 'mongoose';
+import { Product, User } from 'src/common/schemas';
+import { Document, Model } from 'mongoose';
 
 @Injectable()
 export class ProductService {
   constructor(
     @InjectModel(Product.name) private productModel: Model<Product>,
   ) {}
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  async create(createProductDto: CreateProductDto, user: User & Document) {
+    try {
+      const product = await this.productModel.create({
+        ...createProductDto,
+        authorId: user._id,
+      });
+      return product;
+    } catch (error) {
+      console.error(error);
+      throw new ForbiddenException('Something went wrong\n' + error);
+    }
   }
 
-  findAll() {
-    return `This action returns all product`;
+  async findAll() {
+    return await this.productModel.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOneByArticle(article: string) {
+    const product = await this.productModel.findOne({ article });
+    if (!product) {
+      throw new ForbiddenException('Product not found');
+    }
+    return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async findOneById(id: string) {
+    const product = await this.productModel.findById(id);
+    if (!product) {
+      throw new ForbiddenException('Product not found');
+    }
+    return product;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async update(
+    id: string,
+    updateProductDto: UpdateProductDto,
+    user: User & Document,
+  ) {
+    const product = await this.productModel.findById(id);
+    if (!product) {
+      throw new NotFoundException('Product not found');
+    }
+    if (product.authorId?.toString() !== user._id.toString()) {
+      throw new ForbiddenException(
+        'You are not allowed to update this product',
+      );
+    }
+    try {
+      const updatedProduct = await this.productModel.findByIdAndUpdate(
+        id,
+        updateProductDto,
+        { new: true },
+      );
+      return updatedProduct;
+    } catch (error) {
+      console.error(error);
+      throw new ForbiddenException('Something went wrong' + error);
+    }
+  }
+
+  async remove(id: string, user: User & Document) {
+    const product = await this.productModel.findById(id);
+    if (!product) {
+      throw new NotFoundException('Product not found');
+    }
+    if (product.authorId.toString() !== user._id.toString()) {
+      throw new ForbiddenException(
+        'You are not allowed to remove this product',
+      );
+    }
+    return await this.productModel.findByIdAndDelete(id);
   }
 }
