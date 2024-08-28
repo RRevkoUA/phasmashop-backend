ab9ef50211a5e1e420dcfaf24c25e71d6e4e075b
RRevkoUA <rustamanrevko@gmail.com>
2024-08-28 20:28:26 +0300

    add new enums

---
+++ src/common/enums/delivery-status.enum.ts
@@ -0,0 +1,7 @@
+export enum DeliveryStatus {
+  WAITING = 'waiting',
+  IN_PROGRESS = 'in_progress',
+  DELIVERED = 'delivered',
+  CANCELED_BY_CLIENT = 'canceled_by_client',
+  CANCELED_BY_SELLER = 'canceled_by_seller',
+}
