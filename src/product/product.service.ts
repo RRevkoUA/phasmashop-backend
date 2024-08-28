98e90a656dbfc87b58b30ab0ee40c2455f5f5896
RRevkoUA <rustamanrevko@gmail.com>
2024-08-28 16:44:58 +0300

    add productModel to service

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto, UpdateProductDto } from './dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Product } from 'src/common/schemas';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class ProductService {
+  constructor(
+    @InjectModel(Product.name) private productModel: Model<Product>,
+  ) {}
   create(createProductDto: CreateProductDto) {
     return 'This action adds a new product';
   }
