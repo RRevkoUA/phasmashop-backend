230d7e1df8a0d80393f2f79876a4bf296d98acb7
RRevkoUA <rustamanrevko@gmail.com>
2024-08-28 20:49:01 +0300

    add Mongoose module to module

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Order, OrderSchema } from 'src/common/schemas/Order.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Order.name,
+        schema: OrderSchema,
+      },
+    ]),
+  ],
   controllers: [OrderController],
   providers: [OrderService],
 })
