706194129e43e865840a18c407aa6889cc4d8df8
RRevkoUA <rustamanrevko@gmail.com>
2024-08-29 10:46:32 +0300

    update orders crud

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,34 +1,57 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { OrderService } from './order.service';
-import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
+import { CreateOrderDto, UpdateOrderDto } from './dto';
+import { ApiTags } from '@nestjs/swagger';
+import { ApiAccessAuth, GetUser } from 'src/common/decorator';
+import { JwtGuard } from 'src/common/guard';
+import { User } from 'src/common/schemas';
+import { Document } from 'mongoose';
 
+@UseGuards(JwtGuard)
+@ApiAccessAuth()
+@ApiTags('Order')
 @Controller('order')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
 
   @Post()
-  create(@Body() createOrderDto: CreateOrderDto) {
-    return this.orderService.create(createOrderDto);
+  create(
+    @Body() createOrderDto: CreateOrderDto,
+    @GetUser() user: User & Document,
+  ) {
+    return this.orderService.create(createOrderDto, user);
   }
 
-  @Get()
+  @Get('all')
   findAll() {
     return this.orderService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.orderService.findOne(+id);
+  findOne(@Param('id') id: string, @GetUser() user: User & Document) {
+    return this.orderService.findOne(id, user);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
-    return this.orderService.update(+id, updateOrderDto);
+  update(
+    @Param('id') id: string,
+    @Body() updateOrderDto: UpdateOrderDto,
+    @GetUser() user: User & Document,
+  ) {
+    return this.orderService.update(id, updateOrderDto, user);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.orderService.remove(+id);
+  remove(@Param('id') id: string, @GetUser() user: User & Document) {
+    return this.orderService.remove(id, user);
   }
 }
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,26 +1,68 @@
-import { Injectable } from '@nestjs/common';
-import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { CreateOrderDto, UpdateOrderDto } from './dto';
+import { Order } from 'src/common/schemas/Order.schema';
+import { Document, Model } from 'mongoose';
+import { GetUser } from 'src/common/decorator';
+import { User } from 'src/common/schemas';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class OrderService {
-  create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
+  constructor(@InjectModel(Order.name) private orderModel: Model<Order>) {}
+  async create(
+    createOrderDto: CreateOrderDto,
+    @GetUser() user: User & Document,
+  ) {
+    try {
+      const order = await this.orderModel.create({
+        ...createOrderDto,
+        userId: user._id,
+      });
+      return order;
+    } catch (error) {
+      throw new ForbiddenException('Something went wrong');
+    }
   }
 
-  findAll() {
-    return `This action returns all order`;
+  async findAll() {
+    return await this.orderModel.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} order`;
+  async findOne(id: string, user: User & Document) {
+    const order = await this.orderModel.findById(id);
+    if (!order) {
+      throw new ForbiddenException('Order not found');
+    }
+    if (order.userId.toString() !== user._id.toString()) {
+      throw new ForbiddenException('You are not allowed to view this order');
+    }
+    return order;
   }
 
-  update(id: number, updateOrderDto: UpdateOrderDto) {
-    return `This action updates a #${id} order`;
+  async update(
+    id: string,
+    updateOrderDto: UpdateOrderDto,
+    user: User & Document,
+  ) {
+    const order = await this.orderModel.findById(id);
+    if (!order) {
+      throw new ForbiddenException('Order not found');
+    }
+    if (order.userId.toString() !== user._id.toString()) {
+      throw new ForbiddenException('You are not allowed to update this order');
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} order`;
+  async remove(id: string, user: User & Document) {
+    const order = await this.orderModel.findById(id);
+
+    if (order.userId.toString() !== user._id.toString()) {
+      throw new ForbiddenException('You are not allowed to update this order');
+    }
+    try {
+      return await this.orderModel.deleteOne({ _id: id });
+    } catch (error) {
+      throw new ForbiddenException('You are not allowed to delete this order');
+    }
   }
 }
