b6760be29dc1ed42bb0f3e8eca9e020f2fac68bb
RRevkoUA <rustamanrevko@gmail.com>
2024-08-30 14:47:26 +0300

    replace swagger decorator to Role decorator

--- src/common/decorator/role.decorator.ts
+++ src/common/decorator/role.decorator.ts
@@ -1,9 +1,11 @@
 import { applyDecorators, SetMetadata, UseGuards } from '@nestjs/common';
 import { RoleEnum } from '../enums';
 import { JwtGuard, RoleGuard } from '../guard';
+import { ApiAccessAuth } from './api-access.decorator';
 
 export const Role = (role: RoleEnum) => {
   return applyDecorators(
+    ApiAccessAuth(),
     SetMetadata('role', role),
     UseGuards(JwtGuard, RoleGuard),
   );
