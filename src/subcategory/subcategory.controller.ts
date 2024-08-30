ab3aa79ac2c2f768a625503dbc750c2786fa6f27
RRevkoUA <rustamanrevko@gmail.com>
2024-08-30 15:03:44 +0300

    Set roles to Subcategory

--- src/subcategory/subcategory.controller.ts
+++ src/subcategory/subcategory.controller.ts
@@ -6,39 +6,36 @@ import {
   Patch,
   Param,
   Delete,
-  UseGuards,
 } from '@nestjs/common';
 import { SubcategoryService } from './subcategory.service';
 import { CreateSubcategoryDto } from './dto/create-subcategory.dto';
 import { UpdateSubcategoryDto } from './dto/update-subcategory.dto';
 import { ApiTags } from '@nestjs/swagger';
-import { ApiAccessAuth } from 'src/common/decorator';
-import { JwtGuard } from 'src/common/guard';
+import { RoleEnum } from 'src/common/enums';
+import { Role } from 'src/common/decorator';
 
 @ApiTags('Subcategory')
+@Role(RoleEnum.ADMIN)
 @Controller('subcategory')
 export class SubcategoryController {
   constructor(private readonly subcategoryService: SubcategoryService) {}
 
-  @ApiAccessAuth()
-  @UseGuards(JwtGuard)
   @Post()
   create(@Body() createSubcategoryDto: CreateSubcategoryDto) {
     return this.subcategoryService.create(createSubcategoryDto);
   }
 
+  @Role(RoleEnum.USER)
   @Get()
   findAll() {
     return this.subcategoryService.findAll();
   }
-
+  @Role(RoleEnum.USER)
   @Get(':subcategory')
   findOne(@Param('subcategory') subcategoryName: string) {
     return this.subcategoryService.findOne(subcategoryName);
   }
 
-  @ApiAccessAuth()
-  @UseGuards(JwtGuard)
   @Patch(':subcategory')
   update(
     @Param('subcategory') subcategoryName: string,
@@ -50,8 +47,6 @@ export class SubcategoryController {
     );
   }
 
-  @ApiAccessAuth()
-  @UseGuards(JwtGuard)
   @Delete(':subcategory')
   remove(@Param('subcategory') subcategoryName: string) {
     return this.subcategoryService.remove(subcategoryName);
