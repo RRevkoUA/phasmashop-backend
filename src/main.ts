776e6b41b7f4d32dfaceccb4b3b0650fffa5d84d
RRevkoUA <rustamanrevko@gmail.com>
2024-09-01 13:23:29 +0300

    add middleware to main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { SwaggerModule } from '@nestjs/swagger/dist/swagger-module';
 import { DocumentBuilder } from '@nestjs/swagger/dist/document-builder';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import {
+  EncryptionMiddleware,
+  DecryptionMiddleware,
+} from './common/middlewares';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const config = new DocumentBuilder()
@@ -14,6 +19,7 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+
   app.setGlobalPrefix(process.env.APP_GLOBAL_PREFIX);
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup(process.env.APP_SWAGGER_PATH, app, document);
@@ -28,6 +34,13 @@ async function bootstrap() {
     origin: process.env.APP_ORIGIN,
     credentials: true,
   });
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.use(new EncryptionMiddleware().use);
+  app.use(new DecryptionMiddleware().use);
+
   await app.listen(process.env.APP_PORT);
 }
 bootstrap();
