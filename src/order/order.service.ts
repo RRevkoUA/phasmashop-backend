61c57accf782fdaf86cc391b394dc85e786ba033
RRevkoUA <rustamanrevko@gmail.com>
2024-09-05 11:57:48 +0300

    add some order logs

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,4 +1,4 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable, Logger } from '@nestjs/common';
 import { CreateOrderDto, UpdateOrderDto } from './dto';
 import { Order } from 'src/common/schemas/Order.schema';
 import { Document, Model } from 'mongoose';
@@ -8,6 +8,7 @@ import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class OrderService {
+  readonly logger = new Logger(OrderService.name);
   constructor(@InjectModel(Order.name) private orderModel: Model<Order>) {}
   async create(
     createOrderDto: CreateOrderDto,
@@ -20,6 +21,7 @@ export class OrderService {
       });
       return order;
     } catch (error) {
+      this.logger.error('Something went wrong');
       throw new ForbiddenException('Something went wrong');
     }
   }
@@ -31,9 +33,11 @@ export class OrderService {
   async findOne(id: string, user: User & Document) {
     const order = await this.orderModel.findById(id);
     if (!order) {
+      this.logger.error('Order not found');
       throw new ForbiddenException('Order not found');
     }
     if (order.userId.toString() !== user._id.toString()) {
+      this.logger.error('You are not allowed to view this order');
       throw new ForbiddenException('You are not allowed to view this order');
     }
     return order;
@@ -46,22 +50,30 @@ export class OrderService {
   ) {
     const order = await this.orderModel.findById(id);
     if (!order) {
+      this.logger.error('Order not found');
       throw new ForbiddenException('Order not found');
     }
     if (order.userId.toString() !== user._id.toString()) {
+      this.logger.error('You are not allowed to update this order');
       throw new ForbiddenException('You are not allowed to update this order');
     }
+
+    return await this.orderModel.findByIdAndUpdate(id, updateOrderDto, {
+      new: true,
+    });
   }
 
   async remove(id: string, user: User & Document) {
     const order = await this.orderModel.findById(id);
 
     if (order.userId.toString() !== user._id.toString()) {
+      this.logger.error('You are not allowed to delete this order');
       throw new ForbiddenException('You are not allowed to update this order');
     }
     try {
       return await this.orderModel.deleteOne({ _id: id });
     } catch (error) {
+      this.logger.error('You are not allowed to delete this order');
       throw new ForbiddenException('You are not allowed to delete this order');
     }
   }
