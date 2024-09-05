916fdced1c526414d197db5ae88eed1f47a31763
RRevkoUA <rustamanrevko@gmail.com>
2024-09-05 12:40:19 +0300

    update tag name's dto's

--- src/tag/dto/create-tag.dto.ts
+++ src/tag/dto/create-tag.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreateTagDto {
   @ApiProperty({
@@ -8,6 +8,8 @@ export class CreateTagDto {
     required: true,
     default: 'Tag name',
   })
+  @MinLength(2)
+  @MaxLength(15)
   @IsString()
   name: string;
 }
