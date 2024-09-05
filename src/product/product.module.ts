9c23431c424ab47adec2a219ffd8e016eeec0f1c
RRevkoUA <rustamanrevko@gmail.com>
2024-09-05 22:34:09 +0300

    add subcategory, tags, characteristics for product validation

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -4,11 +4,17 @@ import { ProductController } from './product.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Product, ProductSchema } from 'src/common/schemas';
 import { ImageModule } from 'src/image/image.module';
+import { SubcategoryModule } from 'src/subcategory/subcategory.module';
+import { CharacteristicModule } from 'src/characteristic/characteristic.module';
+import { TagModule } from 'src/tag/tag.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
     ImageModule,
+    SubcategoryModule,
+    CharacteristicModule,
+    TagModule,
   ],
   controllers: [ProductController],
   providers: [ProductService],
