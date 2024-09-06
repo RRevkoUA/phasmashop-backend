62e01df16f37f32bd8e43f0c5d2c0236d2c9159d
RRevkoUA <rustamanrevko@gmail.com>
2024-09-06 22:16:42 +0300

    Remove login validation from signin DTO

--- src/auth/dto/signin-auth.dto.ts
+++ src/auth/dto/signin-auth.dto.ts
@@ -1,13 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+import { IsString, MinLength } from 'class-validator';
 
 export class SigninAuthDto {
   @IsString()
   @MinLength(4)
-  @MaxLength(16)
-  @Matches(/^[a-zA-Z0-9]*$/, {
-    message: 'only letters and numbers allowed',
-  })
   @ApiProperty({
     default: 'usernameOrEmail',
     description: 'may be username, or email',
