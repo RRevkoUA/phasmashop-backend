0e63aed3cbc33e4297d4c81c59fac60003c567ec
RRevkoUA <rustamanrevko@gmail.com>
2024-09-09 18:57:07 +0300

    update subcategory seeder. Fix one in category array bug

--- src/common/seeders/subcategory.seeder.ts
+++ src/common/seeders/subcategory.seeder.ts
@@ -3,22 +3,25 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Subcategory } from '../schemas';
 import { faker } from '@faker-js/faker';
+import { CreateSubcategoryDto } from 'src/subcategory/dto';
+import { SubcategoryService } from 'src/subcategory/subcategory.service';
 
 @Injectable()
 export class SubcategorySeed {
   constructor(
     @InjectModel(Subcategory.name) private subcategoryModel: Model<Subcategory>,
+    private readonly subcategoryService: SubcategoryService,
   ) {}
 
   async seed(amount: number, category: string): Promise<string[]> {
     return new Promise(async (resolve, reject) => {
       while (amount) {
-        const subcategory = new this.subcategoryModel({
+        const subcategoryDto: CreateSubcategoryDto = {
           name: faker.lorem.word({ length: { min: 3, max: 50 } }),
           category,
           isAvailable: true,
-        });
-        await subcategory.save();
+        };
+        await this.subcategoryService.create(subcategoryDto);
         amount--;
         if (!amount) {
           return resolve(await this.subcategoryModel.distinct('name'));
