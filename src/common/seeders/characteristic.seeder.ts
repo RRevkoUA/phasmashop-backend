748605cc6c955b9f3c883c0626413507323cf0aa
RRevkoUA <rustamanrevko@gmail.com>
2024-09-12 18:16:55 +0300

    add characteristic seeder

---
+++ src/common/seeders/characteristic.seeder.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Characteristic } from '../schemas';
+import { Model } from 'mongoose';
+import { faker } from '@faker-js/faker';
+
+@Injectable()
+export class CharacteristicSeed {
+  constructor(
+    @InjectModel(Characteristic.name)
+    private characteristicModel: Model<Characteristic>,
+  ) {}
+
+  async seed(amount: number): Promise<string[]> {
+    return new Promise(async (resolve, reject) => {
+      while (amount) {
+        const characteristic = new this.characteristicModel({
+          name: faker.lorem.word({ length: { min: 3, max: 30 } }),
+          isAvailable: true,
+        });
+        await characteristic.save();
+        amount--;
+        if (!amount) {
+          return resolve(await this.characteristicModel.distinct('name'));
+        }
+      }
+      return resolve([]);
+    });
+  }
+
+  async clear() {
+    return await this.characteristicModel.deleteMany({});
+  }
+}
