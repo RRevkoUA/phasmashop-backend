f58dadeeaf2c343f72615b9d9dbb0f84347b202a
RRevkoUA <rustamanrevko@gmail.com>
2024-11-04 00:16:36 +0200

    fix commentary test

--- test/e2e/commentary.e2e-spec.ts
+++ test/e2e/commentary.e2e-spec.ts
@@ -2,15 +2,17 @@ import { faker } from "@faker-js/faker";
 import { INestApplication } from "@nestjs/common";
 import { Tokens } from "src/auth/types";
 import { CreateCommentaryDto } from "src/commentary/dto";
-import { UserSeed } from "src/common/seeders";
+import { UserSeed, ProductSeed} from "src/common/seeders";
 import { createTestingModule, TestTemplates } from '../test-utils';
 import { RoleEnum } from "src/common/enums";
 import * as pactum from "pactum";
+import { ProductService } from "src/product/product.service";
 
 describe('Commentary E2E Test', () => {
   let app: INestApplication;
   let users: { [key: string]: Tokens } = {};
   let commentaries: string[];
+  let product: any;
   let test: TestTemplates;
 
   const port = Number.parseInt(process.env.APP_PORT) + 60;
@@ -26,7 +28,10 @@ describe('Commentary E2E Test', () => {
     app = appInstance;
     users = await app.get(UserSeed).seedRoles();
     // commentaries = await app.get(CommentarySeed).seed(10);
-    
+    product = await app.get(ProductSeed).seed(1);
+
+    dto.product = product[0]._id;
+    console.error(await app.get(ProductService).findAll());
     test = new TestTemplates(host, uri, users, dto);
     pactum.request.setBaseUrl(host);
   });
